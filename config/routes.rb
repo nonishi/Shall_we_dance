5dbb8973b3faf3afca2d79da0d4af51d07da71fc
nonishi <g.nonono24@gmail.com>
2021-08-06 10:52:56 +0000

    [Add]routes

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,14 @@ Rails.application.routes.draw do
     registrations: 'admins/registrations'
   }
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  
+
   root to: 'homes#top'
   get 'homes/about' => 'homes#about'
+  
+  
+
+  resources :users, only: [:index, :show, :edit, :update]
+  resources :posts, except: [:new]
+  resources :favorites, only: [:index, :create, :destroy]
+  resources :chats, only: [:show, :create]
 end
