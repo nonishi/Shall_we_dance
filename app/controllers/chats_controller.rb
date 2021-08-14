7f0d998f1146219ebd93687a58443da460133b47
nonishi <g.nonono24@gmail.com>
2021-08-14 08:17:53 +0000

    [Add]DM機能

--- app/controllers/chats_controller.rb
+++ app/controllers/chats_controller.rb
@@ -1,2 +1,31 @@
 class ChatsController < ApplicationController
+  def show
+    @user = User.find(params[:id])
+    rooms = current_user.room_users.pluck(:room_id)
+    room_users = RoomUser.find_by(user_id: @user.id, room_id: rooms)
+
+    unless room_users.nil?
+      @room = room_users.room
+    else
+      @room = Room.new
+      @room.save
+      RoomUser.create(user_id: current_user.id, room_id: @room.id)
+      RoomUser.create(user_id: @user.id, room_id: @room.id)
+    end
+    
+    @chats = @room.chats
+    @chat = Chat.new(room_id: @room.id)
+  end
+
+  def create
+    @chat = current_user.chats.new(chat_params)
+    @chat.save
+    redirect_to request.referer
+  end
+
+  private
+
+  def chat_params
+    params.require(:chat).permit(:message, :room_id)
+  end
 end
