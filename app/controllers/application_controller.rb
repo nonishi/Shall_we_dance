aadad246d8103dc0f08a9cc196aa641a3f87b077
nonishi <g.nonono24@gmail.com>
2021-08-21 13:10:17 +0000

    [Fix]ログイン後の遷移先

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,7 @@
 class ApplicationController < ActionController::Base
-
+  
   def after_sign_in_path_for(resource)
-    users_sign_up_complete_path(resource)
+    user_path(current_user)
   end
 
 end
